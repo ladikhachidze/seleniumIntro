ff53884e9e0d1871050af150d00888550ae2d231
Ladi_Cydeo <ladikhachidze@gmail.com>
2023-08-20 18:55:33 -0400

    DropDown Locator

--- src/main/java/org/example/UpdatedDropDowns.java
+++ src/main/java/org/example/UpdatedDropDowns.java
@@ -18,10 +18,25 @@ public class UpdatedDropDowns {
         webDriver.get("https://spicejet.com");
         webDriver.findElement(By.xpath("(//div[@class='css-1dbjc4n'])[57]")).click();
         Thread.sleep(2000);
-        webDriver.findElement(By.xpath("//div[@class='css-1dbjc4n r-k8qxaj r-d9fdf6']//div[1]//div[2]//div[3]")).click();
-        webDriver.findElement(By.xpath("//div[@class='css-1dbjc4n r-1awozwy r-19m6qjp r-z2wwpe r-1loqt21 r-18u37iz r-1777fci r-d9fdf6 r-1w50u8q r-ah5dr5 r-1otgn73']")).click();
 
-       webDriver.findElement(By.xpath("(//div[@class='css-1dbjc4n r-1awozwy r-19m6qjp r-y47klf r-1loqt21 r-eu3ka r-1777fci r-1otgn73 r-eafdt9 r-1i6wzkk r-lrvibr r-1aockid'])[2]")).click();
+
+        System.out.println(webDriver.findElement(By.xpath("(//div[@class='css-1dbjc4n'])[57]")).getText());
+        int i = 1;
+        while (i<5){
+            webDriver.findElement(By.xpath("//div[@class='css-1dbjc4n r-k8qxaj r-d9fdf6']//div[1]//div[2]//div[3]")).click();
+            i++;
+        }
+        Thread.sleep(2000);
+
+
+        for (int j = 0; j < 5; j++) {
+            webDriver.findElement(By.xpath("(//*[name()='svg'][@data-testid='svg-img'])[20]")).click();
+        }
+
+        System.out.println(webDriver.findElement(By.xpath("(//div[@class='css-1dbjc4n'])[57]")).getText());
+
+        Thread.sleep(2000);
+        webDriver.findElement(By.cssSelector(".css-1dbjc4n.r-1awozwy.r-19m6qjp.r-z2wwpe.r-1loqt21.r-18u37iz.r-1777fci.r-d9fdf6.r-1w50u8q.r-ah5dr5.r-1otgn73")).click();
         Thread.sleep(2000);
         webDriver.close();
 
