639e5e169259113bcc2a76df23aaecd056946dff
Ladi_Cydeo <ladikhachidze@gmail.com>
2023-08-20 18:23:06 -0400

    DropDown Locator

--- src/main/java/org/example/UpdatedDropDowns.java
+++ src/main/java/org/example/UpdatedDropDowns.java
@@ -1,12 +1,13 @@
 package org.example;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class UpdatedDropDowns {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         System.setProperty("webdriver.chrome.driver","C:\\Users\\Ladi\\Desktop\\chromedriver-win64\\chromedriver.exe");
 
@@ -15,6 +16,15 @@ public class UpdatedDropDowns {
         WebDriver webDriver = new ChromeDriver(opt);
         webDriver.manage().window().maximize();
         webDriver.get("https://spicejet.com");
+        webDriver.findElement(By.xpath("(//div[@class='css-1dbjc4n'])[57]")).click();
+        Thread.sleep(2000);
+        webDriver.findElement(By.xpath("//div[@class='css-1dbjc4n r-k8qxaj r-d9fdf6']//div[1]//div[2]//div[3]")).click();
+        webDriver.findElement(By.xpath("//div[@class='css-1dbjc4n r-1awozwy r-19m6qjp r-z2wwpe r-1loqt21 r-18u37iz r-1777fci r-d9fdf6 r-1w50u8q r-ah5dr5 r-1otgn73']")).click();
+
+       webDriver.findElement(By.xpath("(//div[@class='css-1dbjc4n r-1awozwy r-19m6qjp r-y47klf r-1loqt21 r-eu3ka r-1777fci r-1otgn73 r-eafdt9 r-1i6wzkk r-lrvibr r-1aockid'])[2]")).click();
+        Thread.sleep(2000);
+        webDriver.close();
+
 
 
     }
