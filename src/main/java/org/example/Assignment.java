27aabfcafa48cffda6a63952a8b6cfc102bd2a96
Ladi_Cydeo <ladikhachidze@gmail.com>
2023-08-23 19:45:27 -0400

    AutoSuggestive

---
+++ src/main/java/org/example/Assignment.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+
+public class Assignment {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\Ladi\\Desktop\\chromedriver-win64\\chromedriver.exe");
+
+        ChromeOptions opt = new ChromeOptions();
+        opt.addArguments("--remote-allow-origins=*");
+        WebDriver webDriver = new ChromeDriver(opt);
+        webDriver.manage().window().maximize();
+        webDriver.get("https://rahulshettyacademy.com/AutomationPractice/");
+
+        webDriver.findElement(By.cssSelector("input[id*='checkBoxOption1']")).click();
+        Assert.assertFalse(webDriver.findElement(By.cssSelector("input[id*='checkBoxOption1']")).isSelected());
+        Assert.assertFalse(true);
+        System.out.println(webDriver.findElements(By.cssSelector("input[type$='checkbox']")).size());
+
+
+    }
+}
