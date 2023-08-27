b29e5c250f84026320faacf4359ecb74f9297a55
Ladi_Cydeo <ladikhachidze@gmail.com>
2023-08-26 21:04:49 -0400

    Add cart

--- src/main/java/org/example/FanctionalTesting.java
+++ src/main/java/org/example/FanctionalTesting.java
@@ -5,19 +5,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
 
 public class FanctionalTesting {
 
     public static void main(String[] args) throws InterruptedException {
-
-
-
-
-
-
 
 
         System.setProperty("webdriver.chrome.driver","C:\\Users\\Ladi\\Desktop\\chromedriver-win64\\chromedriver.exe");
@@ -32,16 +29,35 @@ public class FanctionalTesting {
 
         Thread.sleep(3000);
 
+        WebDriverWait w = new WebDriverWait(webDriver,Duration.ofSeconds(5));
 
         String[] itemsNeeded= {"Cucumber","Brocolli","Beetroot"};
 
         addItems(webDriver,itemsNeeded);
 
+        webDriver.findElement(By.cssSelector("img[alt='Cart']")).click();
+
+        webDriver.findElement(By.xpath("//button[contains(text(),'PROCEED TO CHECKOUT')]")).click();
+
+        w.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input.promoCode")));
+
+
+
+        webDriver.findElement(By.cssSelector("input.promoCode")).sendKeys("rahulshettyacademy");
+
+        webDriver.findElement(By.cssSelector("button.promoBtn")).click();
+
+//explicit wait
+
+
+
+        w.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span.promoInfo")));
+
+        System.out.println(webDriver.findElement(By.cssSelector("span.promoInfo")).getText());
+
     }
 
-    public static  void addItems(WebDriver driver,String[] itemsNeeded)
-
-    {
+    public static  void addItems(WebDriver driver,String[] itemsNeeded){
 
         int j=0;
 
